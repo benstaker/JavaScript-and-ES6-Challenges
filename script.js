988fb31d903ffebd949fe1531c6efd8082a12feb
Ben Staker <ben@lychee.technology>
2021-01-05 10:37:59 +0000

    Exercise 6

--- script.js
+++ script.js
@@ -4,13 +4,11 @@ Use arrow functions instead of functions
 where possible.
 */
 
-"use strict";
+'use strict';
 
-function mult(a, b) {
-  return a * b;
-}
+const mult = (a, b) => a * b;
 
-setTimeout(function() {
-  console.log(mult(5, 10));
+setTimeout(() => {
+    console.log(mult(5, 10));
 }, 1000);
 // 50
