235f84eb03bf13c6e057d1fa18be13b4dbf1add2
Ben Staker <ben@lychee.technology>
2021-01-05 11:22:08 +0000

    Exercise 18

--- script.js
+++ script.js
@@ -3,22 +3,23 @@
 Rewrite code below using ES6 Classes
 */
 
-"use strict";
+'use strict';
 
-var Fruit = function(title, price) {
-  this.title = title;
-  this.price = price;
-};
+class Fruit {
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
 
-Fruit.prototype.priceInfo = function() {
-  return `Price of one ${this.title} is \
-${this.price}$`;
-};
+    priceInfo() {
+        return `Price of one ${this.title} is ${this.price}$`;
+    }
+}
 
-var apple = new Fruit("Apple", 2);
+var apple = new Fruit('Apple', 2);
 console.log(apple.priceInfo());
 // Price of one Apple is 2$
 
-var orange = new Fruit("Orange", 3);
+var orange = new Fruit('Orange', 3);
 console.log(orange.priceInfo());
 // Price of one Orange is 3$
