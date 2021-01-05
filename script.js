6bf3d4932e440f46fd7e643401e68d34045366ba
Ben Staker <ben@lychee.technology>
2021-01-05 11:02:41 +0000

    Exercise 14

--- script.js
+++ script.js
@@ -8,26 +8,29 @@ If input object doesn't have postsQuantity field
 it should get default value 0.
 */
 
-"use strict";
+'use strict';
 
 var person1 = {
-  name: "Mike",
-  info: {
-    country: "Spain",
-    age: 23
-  },
-  postsQuantity: 100
+    name: 'Mike',
+    info: {
+        country: 'Spain',
+        age: 23
+    },
+    postsQuantity: 100
 };
 
 var person2 = {
-  name: "Alice",
-  info: {
-    country: "Italy",
-    age: 25
-  }
+    name: 'Alice',
+    info: {
+        country: 'Italy',
+        age: 25
+    }
 };
 
 // Write function here
+function shortPerson({ name: n, info: { country: c, age: a }, postsQuantity: p = 0 }) {
+    return { n, c, a, p };
+}
 
 console.log(shortPerson(person1));
 // {n: "Mike", c: "Spain", a: 23, p: 100}
