879b9e9ace1c583b68deba45c15b1b55053825db
Ben Staker <ben@lychee.technology>
2021-01-05 11:11:58 +0000

    Exercise 15

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ Count number of the lowercase vowel letters in the string.
 Vowel letters - a, e, i, o, u
 */
 
-"use strict";
+'use strict';
 
 var vowelsCount = 0;
-var vowels = ["a", "e", "i", "o", "u"];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
 
-var str = "Today is best day of my life";
+var str = 'Today is best day of my life';
 
 // Write code here
+vowelsCount = str.split('').reduce((result, item) => {
+    return vowels.includes(item) ? result + 1 : result;
+}, 0);
 
 console.log(vowelsCount);
 // 8
